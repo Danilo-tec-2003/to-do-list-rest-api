16b6ab9aecda6c989407ae4a00898cc7f3d165e5
Danilo Mendes <danilodev.br@gmail.com>
2025-04-08 14:55:28 -0300

    Feature: Controller Improvement

--- src/main/java/me/dio/decola_tech_API_REST_to_do_list/controller/TaskController.java
+++ src/main/java/me/dio/decola_tech_API_REST_to_do_list/controller/TaskController.java
@@ -1,5 +1,7 @@
 package me.dio.decola_tech_API_REST_to_do_list.controller;
 
+import jakarta.validation.Valid;
+import me.dio.decola_tech_API_REST_to_do_list.dto.TaskDTO;
 import me.dio.decola_tech_API_REST_to_do_list.model.Task;
 import me.dio.decola_tech_API_REST_to_do_list.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,17 +12,22 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+// Controlador REST que expõe os endpoints da API para gerenciar tarefas (CRUD).
+
 @RestController
 @RequestMapping("api/tasks")
 public class TaskController {
+
 
     @Autowired
     private TaskService taskService;
 
     @GetMapping
-    public List<Task> getAllTask() {
-        return taskService.getAllTask();
+
+    public ResponseEntity<List<Task>> getAllTask() {
+        return ResponseEntity.ok(taskService.getAllTask());
     }
+
 
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
@@ -29,14 +36,14 @@ public class TaskController {
     }
 
     @PostMapping
-    public ResponseEntity<Task> createTask(@RequestBody Task task) {
-        Task createdTask = taskService.createTask(task);
+    public ResponseEntity<Task> createTask(@RequestBody @Valid TaskDTO taskDTO) {
+        Task createdTask = taskService.createTask(taskDTO);
         return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
-        Task updatedTask = taskService.updateTask(id, taskDetails);
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody @Valid TaskDTO taskDTO) {
+        Task updatedTask = taskService.updateTask(id, taskDTO);
         return ResponseEntity.ok(updatedTask);
     }
 
