16bb364c33a57fe4bb35392204c270d4a08598c1
Danilo Mendes <danilodev.br@gmail.com>
2025-04-08 14:56:49 -0300

    Feature: Repository Improvement

--- src/main/java/me/dio/decola_tech_API_REST_to_do_list/repository/TaskRepository.java
+++ src/main/java/me/dio/decola_tech_API_REST_to_do_list/repository/TaskRepository.java
@@ -4,7 +4,8 @@ import me.dio.decola_tech_API_REST_to_do_list.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// Interface que estende JpaRepository e fornece os métodos de acesso ao banco de dados para tarefas.
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
+//aqui está vazio pois o Spring Data JPÁ jé fornece os métodos básicos como: save(), findById(), findAll(), deleteById(), etc...
 }
