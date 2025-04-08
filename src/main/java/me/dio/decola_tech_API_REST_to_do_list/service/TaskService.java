2ea7d47d42da40582bd2cef1761866b8e50a1c45
Danilo Mendes <danilodev.br@gmail.com>
2025-04-08 14:57:24 -0300

    Feature: Service Improvement

--- src/main/java/me/dio/decola_tech_API_REST_to_do_list/service/TaskService.java
+++ src/main/java/me/dio/decola_tech_API_REST_to_do_list/service/TaskService.java
@@ -1,5 +1,6 @@
 package me.dio.decola_tech_API_REST_to_do_list.service;
 
+import me.dio.decola_tech_API_REST_to_do_list.dto.TaskDTO;
 import me.dio.decola_tech_API_REST_to_do_list.model.Task;
 import me.dio.decola_tech_API_REST_to_do_list.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +9,13 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+// Classe de serviço que contém a lógica de negócio para manipulação de tarefas.
 @Service
 public class TaskService {
 
     @Autowired
     private TaskRepository taskRepository;
+
 
     public List<Task> getAllTask() {
         return taskRepository.findAll();
@@ -22,15 +25,19 @@ public class TaskService {
         return taskRepository.findById(id);
     }
 
-    public Task createTask(Task task) {
+    public Task createTask(TaskDTO taskDTO) {
+        Task task = new Task();
+        task.setTitle(taskDTO.getTitle());
+        task.setDescription(taskDTO.getDescription());
+        task.setCompleted(taskDTO.isCompleted());
         return taskRepository.save(task);
     }
 
-    public Task updateTask(Long id, Task taskDetails) {
+    public Task updateTask(Long id, TaskDTO taskDTO) {
         Task task = taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
-        task.setTitle(taskDetails.getTitle());
-        task.setDescription(taskDetails.getDescription());
-        task.setCompleted(taskDetails.isCompleted());
+        task.setTitle(taskDTO.getTitle());
+        task.setDescription(taskDTO.getDescription());
+        task.setCompleted(taskDTO.isCompleted());
         return taskRepository.save(task);
     }
 
